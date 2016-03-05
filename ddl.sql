16b55733ca9981f2a520b3818ecb5b6e7c77be3e
Trent Schafer <trent.schafer@gmail.com>
2016-03-05 20:31:37 +1100

    Add population projection

--- ddl.sql
+++ ddl.sql
@@ -105,3 +105,39 @@ from
     gdb_ger_fs_population population
     join fed_state_map state_code on (state_code.state_name = population.federal_state);
 /
+
+create or replace view v_germany_projected_population as
+select federal_state, projection_year, year_population
+from gdb_ger_fs_population_future
+unpivot (
+    year_population for projection_year in (
+        YEAR_2015 as '2015',
+        YEAR_2016 as '2016',
+        YEAR_2017 as '2017',
+        YEAR_2018 as '2018',
+        YEAR_2019 as '2019',
+        YEAR_2020 as '2020',
+        YEAR_2021 as '2021',
+        YEAR_2022 as '2022',
+        YEAR_2023 as '2023',
+        YEAR_2024 as '2024',
+        YEAR_2025 as '2025',
+        YEAR_2026 as '2026',
+        YEAR_2027 as '2027',
+        YEAR_2028 as '2028',
+        YEAR_2029 as '2029',
+        YEAR_2030 as '2030',
+        YEAR_2031 as '2031',
+        YEAR_2032 as '2032',
+        YEAR_2033 as '2033',
+        YEAR_2034 as '2034',
+        YEAR_2035 as '2035',
+        YEAR_2036 as '2036',
+        YEAR_2037 as '2037',
+        YEAR_2038 as '2038',
+        YEAR_2039 as '2039',
+        YEAR_2040 as '2040'
+    )
+)
+where population_by_gender = 'total';
+/
